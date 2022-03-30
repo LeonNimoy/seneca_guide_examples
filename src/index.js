5f12343b037266c282161e9244a7ebfb98c007ef
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-03-30 20:41:12 -0300

    feature: send messages chained

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ seneca.add('role:math, cmd:sum', (msg, respond) => {
     respond(null, {answer: sum})
 })
 
-seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, function (err, result) {
-    if (err) return console.error(err)
-    console.log(result)
+seneca.add('role:math, cmd:sum, integer:true', (msg, respond) => {
+    const sum = Math.floor(msg.left) + Math.floor(msg.right)
+    respond(null, {answer: sum})
 })
+
+seneca.add('role:math, cmd:product', (msg, respond) => {
+    const product = msg.left * msg.right
+    respond(null, {answer: product})
+})
+
+seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, console.log)
+    .act({role: 'math', cmd: 'product', left: 2, right: 3}, console.log)
+    .act({role: 'math', cmd: 'sum', left: 4.5, right: 4.5, integer: true}, console.log)
